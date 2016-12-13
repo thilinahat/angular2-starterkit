0e20aedfe68c70048c628d9aac9509b33c33ded9
chamupathi <chamupathi2008@gmail.com>
2016-12-13 19:58:45 +0530

    Client Navigation Routes Changed Client Search

---
+++ client/app/pages/clients/addclient/header/clients.header.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+import { ClientService } from '../../services/client.service'
+
+@Component({
+    moduleId:module.id,
+    selector: 'clientsheader',
+    templateUrl: './clientsheader.template.html',
+
+
+})
+
+export class ClientsHeaderComponent { }
