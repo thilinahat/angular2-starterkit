8fcd42a95da6d23886e4842c28ca1b44fe36a499
chamupathi <chamupathi2008@gmail.com>
2016-12-13 14:34:13 +0530

    mobile navigation

--- client/script.js
+++ client/script.js
@@ -1,8 +1,15 @@
 toggleSideBar = function () {
 
+        //works well when initially loaded with mobile iew
       $('#sideNavBar').toggleClass("display");
-     $('body').toggleClass("display");
+    $('body').toggleClass("display");
 
-    console.log($('#sideNavBar').html());
+
+    //$('#sideNavBar').toggleClass("display-mobile-side-nav");
+
+
+   // $("#sideNavBar").animate({width:'toggle'},350);
+    //$('#sideNavBar').css('right','0px');
+
 
 }
