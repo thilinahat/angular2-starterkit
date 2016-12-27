a4b9daa50087e16175aeca53849085f2050e33ad
tharakamd <tharakamd.12@cse.mrt.ac.lk>
2016-12-27 21:37:27 +0530

    solve startup server conneting issue

--- server/routes/MailServerIMAP.js
+++ server/routes/MailServerIMAP.js
@@ -33,51 +33,7 @@ var imap = new Imap({
 function openInbox(cb) {
     imap.openBox('INBOX', true, cb);
 }
-/*
-imap.once('ready', function() {
-    openInbox(function(err, box) {
-        if (err) throw err;
-        var f = imap.seq.fetch('1:3', {
-            bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-            struct: true
-        });
-        f.on('message', function(msg, seqno) {
-            // console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function(stream, info) {
-                var buffer = '';
-                stream.on('data', function(chunk) {
-                    buffer += chunk.toString('utf8');
-                });
-                stream.once('end', function() {
-                    var re = Imap.parseHeader(buffer);
-                    var dat = {
-                        topic: re.subject,
-                        sender: re.from
-                    };
-                    // var inj = JSON.stringify(dat);
-                    data["headers"].push(dat);
 
-                    // console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-                });
-            });
-            msg.once('attributes', function(attrs) {
-           //     console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-            });
-            msg.once('end', function() {
-            //    console.log(prefix + 'Finished');
-            });
-        });
-        f.once('error', function(err) {
-         //   console.log('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-         //   console.log('Done fetching all messages!');
-            imap.end();
-        });
-    });
-});
-*/
 imap.once('error', function(err) {
     console.log(err);
 });
@@ -138,7 +94,4 @@ function getUnreadMessages(callback) {
         });
     });
 }
-
-
-imap.connect();
 module.exports = router;
