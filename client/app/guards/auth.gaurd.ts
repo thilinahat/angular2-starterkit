72fc1b3226517fb9ffccdde15847edfd4d432f2c
IamThilina <333thilina@gmail.com>
2017-01-13 23:25:23 +0530

    AuthGuard Redirection Resolved

--- client/app/guards/auth.gaurd.ts
+++ client/app/guards/auth.gaurd.ts
@@ -50,17 +50,26 @@ export class AuthGuard implements CanActivate {
                 .then(response => {
                     //noinspection TypeScriptUnresolvedFunction
                     let userRole: string =  response.json().userRole;
+                    let authorized = false;
                     authorizedRoles.forEach(authRole =>{
-                        if(authRole == userRole)
+                        if(authRole == userRole) {
+                            authorized = true;
                             resolve(true);
+                        }
                     });
+                    if(!authorized) {
+                        resolve(false);
+                        this.router.navigate(['/login']);
+                    }
                 }, error => {
                     console.log(error.json());
+                    resolve(false);
                     this.router.navigate(['/login']);
                 })
                 .catch((err) => {
                     //console.log(err);
-                    reject(false);
+                    resolve(false);
+                    this.router.navigate(['/login']);
                 })
         })
     }
